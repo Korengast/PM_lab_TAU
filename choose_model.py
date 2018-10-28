6c4065c6919a91d802d8ab94327089fc73d038c4
Korengast <32592798+Korengast@users.noreply.github.com>
2018-10-28 15:37:53 +0200

    Pipline works (after resizing using average pooling layer)

--- choose_model.py
+++ choose_model.py
@@ -7,7 +7,7 @@ from models.BayramogluetAl2015Model import BayramogluetAl2015Model
 from models.transfer_learning_resnet50 import TlResnet50
 from models.transfer_learning_vgg16 import TlVGG16
 
-EPOCHS = 2
+EPOCHS = 5
 BATCH_SIZE = 5
 
 __author__ = "Koren Gast"
